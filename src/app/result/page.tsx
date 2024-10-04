9e0f61da50386dd99b30686e6e5f63549540578b
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-10-04 20:44:43 +0530

    added Flight detail card and it's toggle

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,14 +1,26 @@
 'use client'
 import Header from '@/components/Header';
 import Card from '@/components/Card';
+import { useState } from 'react';
+import FlightDetail from '@/components/FlightDetail';
 const page = () => {
+const [detail ,setdetail]=useState(false);
+
+const toggleDetail=()=>{
+ setdetail((prev) => !prev);
+}
+
 
 
   return (
     <div>
+      {detail ? <FlightDetail detail={detail} setdetail={setdetail} /> : ' '}
       <Header />
       <div className='flex justify-center items-center flex-col pt-6 gap-3'>
-        <Card />
+        <div onClick={toggleDetail}>
+          <Card />
+        </div>
+
         <Card />
         <Card />
         <Card />
---
+++ src/components/FlightDetail.tsx
@@ -0,0 +1,104 @@
+import { LeftArrow } from "@/icons";
+import { LufthansaLogo } from "@/icons";
+const status = [
+  {
+    day: 'Sat 28 Sept 2:15',
+    aiportName: 'DXB Dubai Internationa Airport',
+    layover:'',
+  },
+  {
+    day: 'Sat 28 Sept 2:15',
+    aiportName: 'RUH King Khalid International Airport',
+    layover:'Layover 2h 25m'
+  },
+  
+  {
+    day: 'Sat 28 Sept 2:15',
+    aiportName: 'RUH King Khalid International Airport',
+    layover:''
+  },
+  {
+    day: 'Sat 28 Sept 2:15',
+    aiportName: 'CDG Paris-charles de Gualle Airport',
+    layover:''
+  },
+];
+
+ type Props={
+  detail:boolean | null ,
+  setdetail:(detail:boolean)=>void
+
+ }
+
+const FlightDetail: React.FC<Props> = ({ detail, setdetail }) => {
+  return (
+    <div className='w-[500px] h-[550px] bg-white border-2 border-green-50 absolute right-0 m-4 rounded-md p-3'>
+      <div className='h-24 border-b-2 p-3'>
+        <LeftArrow
+          height={50}
+          weight={50}
+          onClick={() => setdetail(!detail)}
+        />
+        <span>Flight Detail</span>
+      </div>
+      {/* ToDo : flex flex -row */}
+      <div className='flex flex-row'>
+        <div className='pt-5'>
+          {/* stepper  */}
+          {status.map((item) => (
+            <div className='flex flex-row gap-3'>
+              <div className=' flex flex-col justify-center items-center'>
+                <div className='h-[15px] w-[15px] border-2 border-black rounded-full'></div>
+                {/*  if layover is not empty sring then added dotted line instead of solid  line  */}
+
+                <div
+                  className={` ${
+                    item.layover
+                      ? 'border-l-2 border-dashed border-grey-400 h-[100px]'
+                      : 'bg-black w-[2px] h-[50px]'
+                  }`}
+                ></div>
+              </div>
+
+              <div>
+                <span>{item.day}</span>
+                <div className='flex flex-col gap-2'>
+                  <h2>{item.aiportName}</h2>
+                  {item.layover && (
+                    <p className='text-gray-500'>{item.layover}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <div></div>
+          <div></div>
+        </div>
+
+        <div className='h-[200px] w-[200px] flex flex-col pt-5 items-end gap-24'>
+          <div className='flex flex-row gap-3'>
+            <LufthansaLogo style={{ height: 50, width: 50 }} />
+
+            <div>
+              <span>Saudi Arabian Airlines SV553</span>
+              <span>Economy A330</span>
+              <span>Flight time 3h 45m</span>
+            </div>
+          </div>
+          <div className='flex flex-row gap-3'>
+            <LufthansaLogo style={{ height: 50, width: 50 }} />
+
+            <div>
+              <span>Saudi Arabian Airlines SV553</span>
+              <span>Economy A330</span>
+              <span>Flight time 3h 45m</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlightDetail
